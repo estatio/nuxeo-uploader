37ea160071208948eb6bc1572b92c6f341465c8a
Jeroen van der Wal <jeroen@stromboli.it>
2014-08-18 15:54:29 +0200

    Fixed Document creater, now investigates the path where a document should be placed recursively.

--- src/main/java/org/estatio/DocumentCreator.java
+++ src/main/java/org/estatio/DocumentCreator.java
@@ -1,13 +1,15 @@
 package org.estatio;
 
-import java.io.File;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.attribute.standard.Fidelity;
+import javax.swing.text.html.parser.DTD;
 
 import org.nuxeo.ecm.automation.client.Constants;
 import org.nuxeo.ecm.automation.client.Session;
 import org.nuxeo.ecm.automation.client.jaxrs.impl.HttpAutomationClient;
 import org.nuxeo.ecm.automation.client.model.Document;
-import org.nuxeo.ecm.automation.client.model.FileBlob;
 
 public class DocumentCreator {
 
@@ -36,7 +38,98 @@ public class DocumentCreator {
     public void disconnect() {
         client.shutdown();
     }
-  
+
+    public Document find(String path) {
+        try {
+            return (Document) session.newRequest("Document.Fetch")
+                    .setHeader(Constants.HEADER_NX_SCHEMAS, "*")
+                    .set("value", path)
+                    .execute();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void delete(String path) {
+        Document document = find(path);
+        if (document != null) {
+            try {
+                session.newRequest("Document.Delete").setInput(document).execute();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Document findOrCreateParent(String root, String country, String property, String department, String subject, String subSubject) {
+        List<DocumentType> x = new ArrayList<DocumentType>();
+        x.add(new DocumentType("Domain", root));
+        x.add(new DocumentType("Country", country));
+        x.add(new DocumentType("Property", property));
+        x.add(new DocumentType("Department", department));
+        x.add(new DocumentType("Subject", subject));
+        if (subSubject != null) {
+            x.add(new DocumentType("Subject", subSubject));
+        }
+        return findOrCreateParent(x);
+    }
+
+    private Document findOrCreateParent(List<DocumentType> parentList) {
+        StringBuilder bldr = new StringBuilder();
+        for (int j = 0; j <= parentList.size() - 1; j++) {
+            bldr.append("/").append(parentList.get(j).getName());
+        }
+        Document document = find(bldr.toString());
+        if (document != null) {
+            int x = parentList.size() -1;
+            ArrayList<DocumentType> sub = new ArrayList<DocumentCreator.DocumentType>(parentList);
+            sub.remove(sub.size()-1);
+            Document ownParent = findOrCreateParent(sub);
+            document = findOrCreate(ownParent.getPath(), parentList.get(x).getName(), parentList.get(x).getType());            
+        }
+        return document;
+    }
+
+    private class DocumentType {
+        private String type;
+        private String name;
+
+        public DocumentType(String type, String name) {
+            super();
+            this.type = type;
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public Document findOrCreate(String path, String name, String type) {
+        Document root = find(path);
+        Document property = null;
+        if (find(path + "/" + name) == null) {
+            try {
+                property = (Document) session.newRequest("Document.Create")
+                        .setHeader(Constants.HEADER_NX_SCHEMAS, "*")
+                        .setInput(root)
+                        .set("type", type)
+                        .set("name", name)
+                        .set("properties", "dc:title=" + name)
+                        .execute();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return property;
+    }
+
     public void create(ItalyTechnicalDocument doc, String rootPath) throws Exception {
         if (session == null) {
             connect();
@@ -44,54 +137,40 @@ public class DocumentCreator {
         Document root;
         // Fetch the root of Nuxeo repository
         System.out.println(rootPath);
-        //root = (Document) session.newRequest("Document.Fetch").set("value", rootPath).execute();
-//        try{
-//            Document path = (Document)session.newRequest("Document.Fetch").set("value", "RPM").execute();
-//        }catch(Exception e){
-//            
-//        }
-        Document path=null;
-        try {
-            path = (Document) session.newRequest("Document.Fetch").setInput(rootPath).set("type", "Folder").set("name", "RPM").set("properties", "cd:title=RPM").execute();
-        }catch (Exception e) {
-            
-        }
-        if(path == (null)){
-            session.newRequest("Document.Create").setInput(rootPath).set("properties", "dc:title=RPM").set("name", "RPM").set("type", "Folder").execute();
-        }
-        else {
-            root=path;
-        }
-       
+
         // Instantiate a new Document with the simple constructor
         Document document = new Document("myDocument", "File");
         document.set("dc:title", doc.getName());
-        
         document.set("dc:description", "test description");
-        Document loc = (Document)session.newRequest("Document.Fetch").set("value", "RPM").execute();
-        
-        document = (Document) session.newRequest("Document.Create").setHeader(Constants.HEADER_NX_SCHEMAS, "*").setInput(loc).set("type", document.getType()) .set("name",document.getId()).set("properties", document).execute();
-        
+        Document loc = (Document) session.newRequest("Document.Fetch").set("value", "RPM").execute();
+        document = (Document) session.newRequest("Document.Create")
+                .setHeader(Constants.HEADER_NX_SCHEMAS, "*")
+                .setInput(loc)
+                .set("type", document.getType())
+                .set("name", document.getId())
+                .set("properties", document)
+                .execute();
+
         System.out.print(document.getPath());
         // Create a document of File type by setting the parameter
-          //'properties' // with String metadata values delimited by comma ','
-          document = (Document) session.newRequest("Document.Create")
-          .setHeader(Constants.HEADER_NX_SCHEMAS, "*")
-          .setInput(rootPath).set("type", document.getType()) .set("name",
-          document.getId()) .set("properties", document) .execute();
-          
-          // Update the document document = (Document)
-          session.newRequest("Document.Update") .setInput(document)
-          .set("properties", document) .execute();
-          
-          // create a file document //File file = doc.getFile(); //FileBlob fb
-          //= new FileBlob(file);
-          //fb.setMimeType(Files.probeContentType(file.toPath())); // uploading
-          //a file will return null since we used HEADER_NX_VOIDOP
-          //session.newRequest("Blob.Attach") //
-          //.setHeader(Constants.HEADER_NX_VOIDOP, "true") // .setInput(fb)
-          //.set("document", document.getId()) // .execute();
-         
+        // 'properties' // with String metadata values delimited by comma ','
+        document = (Document) session.newRequest("Document.Create")
+                .setHeader(Constants.HEADER_NX_SCHEMAS, "*")
+                .setInput(rootPath).set("type", document.getType()).set("name",
+                        document.getId()).set("properties", document).execute();
+
+        // Update the document document = (Document)
+        session.newRequest("Document.Update").setInput(document)
+                .set("properties", document).execute();
+
+        // create a file document //File file = doc.getFile(); //FileBlob fb
+        // = new FileBlob(file);
+        // fb.setMimeType(Files.probeContentType(file.toPath())); // uploading
+        // a file will return null since we used HEADER_NX_VOIDOP
+        // session.newRequest("Blob.Attach") //
+        // .setHeader(Constants.HEADER_NX_VOIDOP, "true") // .setInput(fb)
+        // .set("document", document.getId()) // .execute();
+
     }
 
 }
--- src/test/java/org/estatio/DocumentCreatorTest.java
+++ src/test/java/org/estatio/DocumentCreatorTest.java
@@ -1,39 +1,58 @@
 package org.estatio;
 
-import java.io.File;
+import static org.junit.Assert.assertNotNull;
+
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DocumentCreatorTest {
 
     private static final String TEST_ROOT = "/Test";
+    private static final String TEST_COUNTRY = "Italy";
+    private static final String TEST_PROPERTY = "RPM";
+    private static final String TEST_SUBJECT = "A";
+
     private static final String TEST_FILE = "\\\\ams-s-storage\\storageroom\\DeleteByUser\\Marc\\TechnicalArchive\\RPM\\RPM 2_Tecnico\\RPM_Indice Archivio Tecnico ITA Unico.xls";
     private DocumentCreator cr;
     private ExcelReader reader;
     private List<ItalyTechnicalDocument> list;
 
-
     @Before
     public void setUp() throws Exception {
         cr = new DocumentCreator("http://ams-s-nuxeo02:8080/nuxeo/site/automation", "Administrator", "Administrator");
-        reader=new ExcelReader();
+        cr.connect();
+        reader = new ExcelReader();
         list = reader.read();
-        
+        cr.delete(TEST_ROOT + TEST_COUNTRY);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cr.disconnect();
     }
 
     @Test
-    public void test() throws Exception {
-        cr.connect();
-        
+    public void test1() throws Exception {
+        assertNotNull(cr.find(TEST_ROOT));
     }
 
     @Test
     public void test2() throws Exception {
-        cr.create(list.get(0), "/Test");
+        cr.findOrCreate(TEST_ROOT, TEST_COUNTRY, "Country");
     }
+
     @Test
-    public void test3() throws Exception{
+    public void test3() throws Exception {
+        cr.findOrCreateParent("Test", "Italy", "RPM", "2", "A", null);
     }
+
+    @Test
+    public void test4() throws Exception {
+        cr.findOrCreate(TEST_ROOT + "/" + TEST_COUNTRY, TEST_PROPERTY, "Property");
+    }
+
 }
